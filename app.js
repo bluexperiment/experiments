90f00de84a3373387cf91f92b619c53db21568eb
charliecortez <bluexperiment@hotmail.com>
2013-12-05 14:34:22 -0500

    test2

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var http = require ( 'http' );
+var http = require('http');
 
-var x = 0 ;
-http.createServer ( función  (req, res) { 
-  x + = 1 ;
-  res.writeHead ( 200 , { 'Content-Type' : 'text / plain' });
-  res.end ( 'Hola Mundo' + x);
-.}) Escuchar ( 8124 );
-console.log ( 'Server que se ejecuta en http://127.0.0.1:8124/' );
+var x = 0;
+http.createServer(function (req, res) {
+  x += 1;
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World ' + x);
+}).listen(8124);
+console.log('Server running at http://127.0.0.1:8124/');
